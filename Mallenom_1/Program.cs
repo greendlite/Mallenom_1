957a416d2f729076c973546296f35471767de0bf
Степан Сажин <greendlite@gmail.com>
2025-12-12 11:22:15 +0300

    Файл конфигурации

--- Mallenom_1/Program.cs
+++ Mallenom_1/Program.cs
@@ -1,4 +1,32 @@
-﻿//--------------------------------
+﻿// Чтение конфигурации из appsettings.json
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------
 //-----------Задание #1-----------
 //--------------------------------
 int a = 5;
@@ -54,23 +82,39 @@ for (int i = 0; i < rows; i++)
 
         if (oddNumbers[i] == 9)
         {
+            //var res1 = x + 0.5;
+            //var res2 = x / res1;
+            //var res3 = Math.Pow(res2, x);
+            //var res4 = Math.Sin(res3);
+            //var res5 = Math.Sin(res4);
+
+            //var res6 = Math.Sin(Math.Sin(Math.Pow(x / (x + 0.5), x)));
+
             k[i, j] = Math.Sin(Math.Sin(Math.Pow(x / (x + 0.5), x)));
         }
         else if (oddNumbers[i] == 5 || oddNumbers[i] == 7 || oddNumbers[i] == 11 || oddNumbers[i] == 15)
         {
-            var value = Math.Tan(2 * x) + 2.0 / 3.0;
-            if (value != 0)
-            {
-                k[i, j] = Math.Pow(0.5 / (Math.Tan(2 * x) + 2.0 / 3.0), Math.Pow(x, 1.0 / 9.0));
-            }
-            else
-            {
-                k[i, j] = 0;
-            }
+            //var res1 = Math.Tan(2.0 * x);
+            //var res2 = res1 + (2.0 / 3.0);
+            //var res3 = 0.5 / res2;
+            //var res4 = Math.Cbrt(Math.Cbrt(x));
+            //var res5 = Math.Pow(Math.Abs(res3), res4); // Модуль, чтобы избежать комплексного числа
+
+            //var res6 = Math.Pow(Math.Abs(0.5 / (Math.Tan(2.0 * x) + 2.0 / 3.0)), Math.Cbrt(Math.Cbrt(x)));
+
+            k[i, j] = Math.Pow(Math.Abs(0.5 / (Math.Tan(2.0 * x) + 2.0 / 3.0)), Math.Cbrt(Math.Cbrt(x)));
         }
         else
         {
-            k[i, j] = Math.Tan(Math.Pow(Math.Pow(Math.E, (1 - x) / Math.PI) / 12, 3));
+            //var res1 = (1.0 - x) / Math.PI;
+            //var res2 = Math.Exp(res1);
+            //var res3 = res2 / 12;
+            //var res4 = Math.Tan(res3);
+            //var res5 = Math.Pow(res4, 3);
+
+            //var res6 = Math.Pow(Math.Tan(Math.Exp((1.0 - x) / Math.PI) / 12), 3);
+
+            k[i, j] = Math.Pow(Math.Tan(Math.Exp((1.0 - x) / Math.PI) / 12), 3);
         }
     }
 }
@@ -82,7 +126,11 @@ for (int i = 0; i < rows; i++)
 {
     for (int j = 0; j < cols; j++)
     {
-        Console.Write(k[i, j] + " ");
+        Console.WriteLine($"k[{i}, {j}]: {k[i, j]}");
     }
     Console.WriteLine();
 }
+
+//--------------------------------
+//-----------Задание #3-----------
+//--------------------------------
